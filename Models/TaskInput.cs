32a20534d06ef65399c726bacdea9461bb9f2f69
emily12jensen <90439014+emily12jensen@users.noreply.github.com>
2022-02-07 07:12:01 -0700

    Add project files.

---
+++ Models/TaskInput.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6.Models
+{
+    public class TaskInput
+    {
+        [Key]
+        [Required]
+        public int TaskID { get; set; }
+        [Required (ErrorMessage ="You must enter a task.")]
+        public string Task { get; set; }
+        public DateTime DueDate { get; set; }
+        [Required (ErrorMessage ="You must specify a Quadrant")]
+        public int Quadrant { get; set; }
+        
+        //foreign Key Relationship
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+        
+        public bool Completed { get; set; }
+
+    }
+}
---
+++ Models/TaskInputContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6.Models
+{
+    public class TaskInputContext : DbContext
+    {
+        public TaskInputContext(DbContexOptions<TaskInputContext> options) : base(options)
+        {
+
+        }
+        //STILL NEED TO FIX THIS!
+       
+        public DbSet<TaskInput> responses { get; set; }
+        public DbSet<Category> Category { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Category>().HasData(
+                new Category { CategoryId = 1, CategoryName = "Home" },
+                new Category { CategoryId = 2, CategoryName = "School" },
+                new Category { CategoryId = 3, CategoryName = "Work" },
+                new Category { CategoryId = 4, CategoryName = "Church" }
+                );
+
+            mb.Entity<TaskInput>().HasData(
+
+                new TaskInput
+                {
+                    TaskID = 1,
+                    Task = "Finish Homework",
+                    DueDate = (2022,07,20,09,15,0),
+                    Quadrant = 1,
+                    CategoryID = 2,
+                    Completed = true                    
+                },
+                new TaskInput
+                {
+                    TaskID = 1,
+                    Task = "Finish Homework",
+                    DueDate = (2022, 07, 20, 09, 15, 0),
+                    Quadrant = 1,
+                    CategoryID = 2,
+                    Completed = true
+                },
+                new TaskInput
+                {
+                    TaskID = 1,
+                    Task = "Finish Homework",
+                    DueDate = (2022, 07, 20, 09, 15, 0),
+                    Quadrant = 1,
+                    CategoryID = 2,
+                    Completed = true
+                }
+                );
+            }
+
+        }
+}
